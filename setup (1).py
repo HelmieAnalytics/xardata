eb8c5f8de5d7ca8e944cac1bc41ed5a8a01a216d
HelmieAnalytics <francoishelmie@hotmail.com>
2024-07-02 23:50:17 +0200

    Update setup.py

--- setup (1).py
+++ setup (1).py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='zaquity',
-    packages=find_packages(include=['zaquity']),
+    name='Zaquitydata',
+    packages=find_packages(include=['Zaquitydata']),
     version='0.1.0',
-    description='My first Python library',
-    author='Helmie Research',
+    description='A stock price data import package',
+    author='Helmie Analytics',
     install_requires=['yfinance','yahooquery==2.3.7'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
