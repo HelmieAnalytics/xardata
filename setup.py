058ae7309a41dcd95eab0b53b32498010b297e9d
HelmieAnalytics <francoishelmie@hotmail.com>
2024-07-03 00:35:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,43 @@ from setuptools import find_packages, setup
 
 setup(
     name='Zaquitydata',
+    
     packages=find_packages(include=['Zaquitydata']),
+    
     version='0.1.0',
+    
     description='A stock price data import package',
+    
     author='Helmie Analytics',
-    install_requires=['yfinance','yahooquery==2.3.7'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
-    test_suite='tests',
+
+    url='https://github.com/HelmieAnalytics/Zaquitydata',
+    
+    long_description= open('README.md').read() #+ '\n\n' + open('CHANGELOG.md').read(),
+    long_description_content_type = "text/markdown",
+
+    install_requires= [
+        
+        'yfinance',
+        'yahooquery~=2.3.7',
+        'stats',
+    ],
+
+    setup_requires= [
+        
+        'pytest-runner'
+    ],
+
+    tests_require= [
+        
+        'pytest==4.4.1'
+    ],
+
+    test_suite= 'tests',
+
+    keywords= [
+        'Data Mining', 
+        'Data Science',
+        'Equities', 
+        'Stocks', 
+    ]
 )
