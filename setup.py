a17dd0791c39e4c89540e9995d9e112d43714f2d
HelmieAnalytics <francoishelmie@hotmail.com>
2024-07-03 14:48:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     
     packages=find_packages(include=['zaquitydata']),
     
-    version='0.1.3',
+    version='0.1.4',
     
     description='A stock price data import package',
     
@@ -16,7 +16,7 @@ setup(
     long_description= open('README.md').read(),#+ '\n\n' + open('CHANGELOG.md').read(),
     long_description_content_type = "text/markdown",
 
-    install_requires= ['yfinance','yahooquery<=2.3.7'],
+    install_requires= ['yfinance','yahooquery<=2.3.7', 'requests==2.22.0`],
 
     setup_requires= ['pytest-runner'],
 
